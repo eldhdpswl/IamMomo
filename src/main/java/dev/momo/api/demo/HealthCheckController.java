a795e4ff59a5d9f7f2cf16f281b1d0571a7edf71
dks <eldhdpswl@gmail.com>
2022-12-25 01:02:26 +0900

    deply test commit

--- src/main/java/dev/momo/api/demo/HealthCheckController.java
+++ src/main/java/dev/momo/api/demo/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController{
-    @GetMapping("/")
+    @GetMapping("/connection-test")
     public String healthCheck(){
         return "The service is up and running...";
     }
