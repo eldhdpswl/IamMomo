6af1b4de613fec2e3cc5243324e52812ea820df3
dks <eldhdpswl@gmail.com>
2023-01-04 00:00:04 +0900

    fix path commit

--- src/main/java/dev/momo/api/demo/HealthCheckController.java
+++ src/main/java/dev/momo/api/demo/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController{
-    @GetMapping("/connectionTest")
+    @GetMapping("/")
     public String healthCheck(){
         return "The service is up and running...";
     }
